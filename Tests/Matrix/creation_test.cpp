c05d709e174c70c7f41f96150b78c5842d4bb6b1
Adam <adam.brz@onet.eu>
2019-12-21 22:04:02 +0100

    Add creation tests. Tests showed that initialization with initializer list causes memory errors

--- Tests/Matrix/creation_test.cpp
+++ Tests/Matrix/creation_test.cpp
@@ -5,13 +5,63 @@
 
 TEST_CASE("Initializing empty matrix", "[Matrix]")
 {
-    Matrix<> matrix(0, 0);
+    SECTION("Matrix with 0 columns and rows")
+    {
+        Matrix<> matrix(0, 0);
 
-    CHECK(matrix.getColumnCount() == 0);
-    CHECK(matrix.getRowCount() == 0);
+        CHECK(matrix.getColumnCount() == 0);
+        CHECK(matrix.getRowCount() == 0);
 
-    CHECK(matrix.hasDeterminant() == true);
-    CHECK(matrix.isInvertible() == false);
+        CHECK(matrix.hasDeterminant() == true);
+        CHECK(matrix.isInvertible() == false);
+    }
+
+    SECTION("Initialization with initializer list")
+    {
+        Matrix<> matrix = {};
+
+        CHECK(matrix.getColumnCount() == 0);
+        CHECK(matrix.getRowCount() == 0);
+
+        CHECK(matrix.hasDeterminant() == true);
+        CHECK(matrix.isInvertible() == false);
+    }/*
+
+    SECTION("Making copy of empty matrix")
+    {
+        Matrix<> original(0, 0);
+        Matrix<> matrix = original;
+
+        CHECK(matrix.getColumnCount() == 0);
+        CHECK(matrix.getRowCount() == 0);
+
+        CHECK(matrix.hasDeterminant() == true);
+        CHECK(matrix.isInvertible() == false);
+    }
+
+    SECTION("Making copy of empty matrix")
+    {
+        Matrix<> original(0, 0);
+        Matrix<> matrix = original;
+
+        CHECK(matrix.getColumnCount() == 0);
+        CHECK(matrix.getRowCount() == 0);
+
+        CHECK(matrix.hasDeterminant() == true);
+        CHECK(matrix.isInvertible() == false);
+    }*/
+
+    /*SECTION("Moving empty matrix")
+    {
+        Matrix<> origin = Matrix<>(0, 0);
+        Matrix<> matrix = std::move(origin);
+
+        CHECK(matrix.getColumnCount() == 0);
+        CHECK(matrix.getRowCount() == 0);
+
+        CHECK(matrix.hasDeterminant() == true);
+        CHECK(matrix.isInvertible() == false);
+    }*/
 }
 
 
