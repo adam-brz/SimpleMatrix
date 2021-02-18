f2aafba95c86f52d427d382dd56e544d6cfd46c3
Andrew2a1 <adam.brz@onet.eu>
2021-02-18 17:06:05 +0100

    Remove unnecessary Axis class

--- matrix.h
+++ matrix.h
@@ -118,23 +118,6 @@ private:
     }
 };
 
-template <typename T = double>
-class Axis
-{
-public:
-    static const Vector<T> X() {
-        return {1, 0, 0};
-    }
-
-    static const Vector<T> Y() {
-        return {0, 1, 0};
-    }
-
-    static const Vector<T> Z() {
-        return {0, 0, 1};
-    }
-};
-
 #include "matrix.cpp"
 
 #endif // MATRIX_H
